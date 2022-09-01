3aabb4f2131e70d9d3c9e1ea607d5aaa36635266
jackjustus <jackjustushome@gmail.com>
2022-09-01 14:02:07 -0700

    yes

---
+++ src/Board.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Board {
+
+
+    boolean[] squares; 
+    ArrayList<Ship>;
+    2d boolean array named Squares
+    Arraylist of ships
+    Initialization method:
+    Loops through the five ships asking if they want the ship to be vertical or horizontal and then asks where they want the ship to be placed. Checks to see if the ship can be placed there.
+    Int checkGuess(int x, int y)
+    For (ships in board)
+    //Checks coordinates against ship’s coordinates
+    Returns
+0 = no hit
+				1 = you hit part of the ship
+				2 = you sunk a ship
+
+
+}
