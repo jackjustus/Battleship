2914940dbd02f59ce1f166e733a3b08abdaeb409
jackjustus <jackjustushome@gmail.com>
2022-09-14 09:28:45 -0700

    Day 5

--- src/Board.java
+++ src/Board.java
@@ -45,72 +45,84 @@ public class Board {
         print("Time to make your ships!\n\n");
         for (int i = 0; i < NUM_SHIPS; i++) {
 
-            // Finding the rotation of the ships before they choose the coordinates
-            // This needs to be done for the simplicity of input validation
-            int rotation = getRotationalValue();
+            // Input validation confirmation
+            // Turns false when the input validation fails.
+            boolean inputWorks = false;
 
-            // This insures that the rotational input is valid
-            while (!inputRotationValidation(rotation))
-                rotation = getRotationalValue();
+            boolean firstRun = true;
 
-            // This int rotation needs to be converted to a boolean so that the x input validation can use it
-            boolean isVertical;
-            if (rotation == 1)
-                isVertical = true;
-            else
-                isVertical = false;
+            while (!inputWorks) {
+
+                if (!firstRun)
+                    print("Sorry, your input is not valid. Try again!");
 
 
-            // We now use this method to retrieve the x value of the ship
-            int x = getValue(i, "x");
-            // And then validate the input
-            // inputXValidation(boolean isVertical, int value, boolean isXValue, int shipLength)
-            while (!inputValidation(isVertical, x, true, shipLengths[i]))
-                x = getValue(i, "x");
+                // Starts true and gets sets false when it fails
+                inputWorks = true;
 
-            // Time for y value
 
-            int y = getValue(i, "y");
+                // Finding the rotation of the ships before they choose the coordinates
+                // This needs to be done for the simplicity of input validation
+                int rotation = getRotationalValue();
 
-            // Input Validation
-            while (!inputValidation(isVertical, y, false, shipLengths[i])) {
+                // This int rotation needs to be converted to a boolean so that the x input validation can use it
+                boolean isVertical;
+                if (rotation == 1)
+                    isVertical = true;
+                else if (rotation == 2)
+                    isVertical = false;
+                else
+                    // This goes to the top of the while loop because they did not put a valid input
+                    continue;
 
-                print("Sorry, but that coordinate is not valid.\nPlease try another y value");
-                y = getValue(i, "y");
+
+                // We now use this method to retrieve the x value of the ship
+                int x = getValue(i, "x");
+
+
+                // Time for y value
+                int y = getValue(i, "y");
+
+
+                // Converting isVertical to a string
+                String verticalString;
+                if (isVertical)
+                    verticalString = "vertical";
+                else
+                    verticalString = "horizontal";
+
+                // At this point we can run input validation for all the inputs.
+                //TODO: Make Input Validation
+
+
+                // Giving the player feedback based on all of their input
+                print("You placed a " + verticalString + " length " + shipLengths[i] + " ship at " + x + ", " + y + "\n");
+
+
+                // Assigning a letter to the ship based on how many times this loop has run
+                String shipLetter = Translate.convert(i);
+
+
+                // Actually making the ship object
+                // int length, int x, int y, boolean isVertical, String letter
+                ships.add(new Ship(shipLengths[i], x - 1, y - 1, isVertical, shipLetter));
+
+
+                // Marking on the board where the ship is placed
+                markBoardShipPlacement(ships.get(i));
+
+
+                printBoard();
+
+                // Differentiating between ship placements
+                print("\n\nSHIP PLACEMENT COMPLETE\n\n");
 
             }
 
-            // Converting isVertical to a string
-            String verticalString;
-            if (isVertical)
-                verticalString = "vertical";
-            else
-                verticalString = "horizontal";
-
-
-            // Giving the player feedback based on all of their input
-            print("You placed a " + verticalString + " length " + shipLengths[i] + " ship at " + x + ", " + y + "\n");
-
-            // Assigning a letter to the ship based on how many times this loop has ran
-            String shipLetter = Translate.convert(i);
-
-
-            // Actually making the ship object
-            // int length, int x, int y, boolean isVertical, String letter
-            ships.add(new Ship(shipLengths[i], x, y, isVertical, shipLetter));
-
-
-            // Marking on the board where the ship is placed
-            markBoardInitial(ships.get(i));
-
-            // Ship(int length, int x, int y, boolean isVertical)
-            //ships.get(i) = new Ship();
-
         }
-
     }
 
-    private void markBoardInitial(Ship s) {
+    private void markBoardShipPlacement(Ship s) {
         // Marking on the board where the ship is placed
 
         int[][] shipPositions = s.getCoordinates();
@@ -125,11 +137,13 @@ public class Board {
             String letter = s.getLetter();
 
             // Setting the position of the ship on the board to the ship's letter
-            squares[x][y] = letter;
+            // The y is vertical so its first
+            squares[y][x] = letter;
 
         }
 
     }
+
 
     private int getRotationalValue() {
 
@@ -170,10 +184,10 @@ public class Board {
         try {
 
             // Printing the board to give the player a point of reference
-            if (typeOfValue.equals("xPre"))
+            if (typeOfValue.equals("x"))
                 printBoard();
 
-            // What is printed and requested differs based on if it is a x or y value
+            // What is printed and requested differs based on if it is an x or y value
             if (typeOfValue.equals("x")) {
 
                 // This is always called first, so it prompts the player to what they are doing
@@ -219,138 +233,164 @@ public class Board {
 
 
     private boolean inputRotationValidation(int input) {
-
-        if (input == 1 || input == 2)
-            return true;
-        return false;
-
+        return input == 1 || input == 2;
     }
 
 
     private boolean inputValidation(boolean isVertical, int value, boolean isXValue, int shipLength) {
+//
+//        //this bit checks to see if it doens't work
+//        int works = 0;
+//
+//
+//        //i do not know how this works or if it works
+//        //The Himalayas of Validation
+//        /*
+//
+//        if (isXValue == true) {
+//            if (isVertical == true) {
+//                for (int i = 0; i < shipLength; i++) {
+//
+//                    for (int j = 0; j < ships.size(); j++) {
+//
+//                        for (int k = 0; k < ships.get(j).getCoordinates().length; k++) {
+//
+//                            if (value == ships.get(j).getCoordinates()[1][k]) {
+//                                works++;
+//                            }
+//                        }
+//                    }
+//                }
+//            } else {
+//                if (value > (10 - shipLength)){
+//                    return false;
+//                }
+//                    for (int i = 0; i < shipLength; i++) {
+//
+//                        for (int j = 0; j < ships.size(); j++) {
+//
+//                            for (int k = 0; k < ships.get(j).getCoordinates().length; k++) {
+//
+//                                if (value == ships.get(j).getCoordinates()[0][k]) {
+//                                    works++;
+//                                }
+//                            }
+//                        }
+//                    }
+//            }
+//        } else {
+//            if (isVertical == true) {
+//                if (value > (10 - shipLength)) {
+//                    return false;
+//                }
+//                for (int i = 0; i < shipLength; i++) {
+//
+//                    for (int j = 0; j < ships.size(); j++) {
+//
+//                        for (int k = 0; k < ships.get(j).getCoordinates().length; k++) {
+//
+//                            if (value == ships.get(j).getCoordinates()[k][1]) {
+//                                works++;
+//                            }
+//                        }
+//                    }
+//                }
+//            } else {
+//                for (int i = 0; i < shipLength; i++) {
+//
+//                    for (int j = 0; j < ships.size(); j++) {
+//
+//                        for (int k = 0; k < ships.get(j).getCoordinates().length; k++) {
+//
+//                            if (value == ships.get(j).getCoordinates()[k][0]) {
+//                                works++;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//         */
+//
+//        //the better validation
+//
+//        for (int i = 0; i < shipLength; i++) {
+//
+//            // chekc show meny ships we have
+//            for (int j = 0; j < ships.size(); j++) {
+//
+//
+//                //checks lengh of the ship
+//                for (int k = 0; k < ships.get(j).getCoordinates().length; k++) {
+//
+//
+//                    //if were chicking a x value we do this
+//                    if (isXValue == true) {
+//
+//                        //now if thre hsip is vertical we do this
+//                        if (isVertical == true) {
+//
+//                            //if the x calue is the same as any of the x values in a ship we move on
+//                            if (value == ships.get(j).getCoordinates()[1][k]) {
+//                                works++;
+//                            }
+//
+//                            //if were checking y value we do this
+//                        } else {
+//
+//                            //if the sip is ges off the edge it returns fales
+//                            if (value > (10 - shipLength)) {
+//                                return false;
+//                            }
+//
+//                            //now if it doesnt go of the edge
+//                            //if the x value is the same as any of the x values we move on
+//                            if (value == ships.get(j).getCoordinates()[0][k]) {
+//                                works++;
+//                            }
+//                        }
+//
+//
+//
+//                        //we do this if the value is for a y
+//                    } else {
+//                        if (isVertical == true) {
+//
+//                            //chekc if go off bored
+//                            if (value > (10 - shipLength)) {
+//                                return false;
+//                            }
+//
+//                            //if y value is same as ships goin verticly we move on
+//                            if (value == ships.get(j).getCoordinates()[k][1]) {
+//                                works++;
+//                            }
+//
+//                            //if y value is same as ships going horizontally we move on
+//                        } else {
+//                            if (value == ships.get(j).getCoordinates()[k][0]) {
+//                                works++;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//
+//
+//        //if the works counter is the same as the ships length then it doesnt work
+//        //if it is not then it does work
+//
+//
+//        //more checking to see if it works
+//        if (works == shipLength) {
+//            return false;
+//        } else {
+//            return true;
+//        }
 
-        //this bit checks to see if it doens't work
-        int works = 0;
-
-
-        //i do not know how this works or if it works
-        //The Himalayas of Validation
-        /*
-
-        if (isXValue == true) {
-            if (isVertical == true) {
-                for (int i = 0; i < shipLength; i++) {
-
-                    for (int j = 0; j < ships.size(); j++) {
-
-                        for (int k = 0; k < ships.get(j).getCoordinates().length; k++) {
-
-                            if (value == ships.get(j).getCoordinates()[1][k]) {
-                                works++;
-                            }
-                        }
-                    }
-                }
-            } else {
-                if (value > (10 - shipLength)){
-                    return false;
-                }
-                    for (int i = 0; i < shipLength; i++) {
-
-                        for (int j = 0; j < ships.size(); j++) {
-
-                            for (int k = 0; k < ships.get(j).getCoordinates().length; k++) {
-
-                                if (value == ships.get(j).getCoordinates()[0][k]) {
-                                    works++;
-                                }
-                            }
-                        }
-                    }
-            }
-        } else {
-            if (isVertical == true) {
-                if (value > (10 - shipLength)) {
-                    return false;
-                }
-                for (int i = 0; i < shipLength; i++) {
-
-                    for (int j = 0; j < ships.size(); j++) {
-
-                        for (int k = 0; k < ships.get(j).getCoordinates().length; k++) {
-
-                            if (value == ships.get(j).getCoordinates()[k][1]) {
-                                works++;
-                            }
-                        }
-                    }
-                }
-            } else {
-                for (int i = 0; i < shipLength; i++) {
-
-                    for (int j = 0; j < ships.size(); j++) {
-
-                        for (int k = 0; k < ships.get(j).getCoordinates().length; k++) {
-
-                            if (value == ships.get(j).getCoordinates()[k][0]) {
-                                works++;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-         */
-
-        //the better validation
-        for (int i = 0; i < shipLength; i++) {
-
-            for (int j = 0; j < ships.size(); j++) {
-
-                for (int k = 0; k < ships.get(j).getCoordinates().length; k++) {
-
-
-                    if (isXValue == true) {
-                        if (isVertical == true) {
-                            if (value == ships.get(j).getCoordinates()[1][k]) {
-                                works++;
-                            }
-                        } else {
-                            if (value > (10 - shipLength)) {
-                                return false;
-                            }
-                            if (value == ships.get(j).getCoordinates()[0][k]) {
-                                works++;
-                            }
-                        }
-
-                    } else {
-                        if (isVertical == true) {
-                            if (value > (10 - shipLength)) {
-                                return false;
-                            }
-                            if (value == ships.get(j).getCoordinates()[k][1]) {
-                                works++;
-                            }
-                        } else {
-                            if (value == ships.get(j).getCoordinates()[k][0]) {
-                                works++;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-
-        //more checking to see if it works
-        if (works == shipLength) {
-            return false;
-        } else {
-            return true;
-        }
-
-
+        return true;
     }
 
 
