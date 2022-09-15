6902716ae961fd293b8ff2a712beeb74cf80c0fb
jackjustus <jackjustushome@gmail.com>
2022-09-15 12:21:47 -0700

    Changes by jackstahl

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,5 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
-
-//Two player objects each with their own board
 public class Player {
 
 
@@ -10,6 +9,7 @@ public class Player {
     private Board b;
     //checks amount of opponents ships sunken
     private int otherPlayerSunken;
+
 
     private boolean numShipsGood;
 
@@ -35,7 +35,7 @@ public class Player {
         name = s.nextLine();
         System.out.println("Excellent.");
 
-        //just for fun
+        //just for jokes
         //asks for number of ships player would like to play with
         //once valid num is given initializes Board "b" based on ships in Board class
         while (!numShipsGood) {
@@ -61,59 +61,64 @@ public class Player {
     //takes coordinates from player and checks if it's a hit, miss, or already guessed
     public void attack(Board b1) {
         boolean attackAvailable = true;
-        boolean secondTime = false;
         int x = 0;
         int y = 0;
-        int xTemp = -1;
-        int yTemp = -1;
-        int timesThru = 0;
         b.printBoard();
 
-        //asks for coordinates of a space the player would like to guess
+        //while attackAvailable is true, the player attacks
         while (attackAvailable) {
-
-            System.out.println(">>Enter the letter coordinate for the row you wish to attack");
-            yTemp = Translate.convert(s.nextLine());
-            while(y == -1) {
-                System.out.println(">>Invalid");
-                System.out.println(">>Enter what letter coordinate you wish to attack");
+                //asks for coordinates of a space the player would like to guess and validates their guess
+                //takes y coordinate (letter)
+                System.out.println(">>Enter the letter coordinate for the row you wish to attack");
                 y = Translate.convert(s.nextLine());
-            }
-            System.out.println(">>Enter the number coordinate for the column you wish to attack");
-            x = s.nextInt();
-            while(x > 10 || x < 1){
-                System.out.println(">>Invalid");
-                System.out.println(">>Enter what number coordinate you wish to attack");
+
+                while (y == -1) {
+                    System.out.println(">>Invalid");
+                    System.out.println(">>Enter what letter coordinate you wish to attack");
+                    y = Translate.convert(s.nextLine());
+                }
+
+                //takes x coordinate (number)
+                System.out.println(">>Enter the number coordinate for the column you wish to attack");
                 x = s.nextInt();
+
+                while (x > 10 || x < 1) {
+                    System.out.println(">>Invalid");
+                    System.out.println(">>Enter the number coordinate for the column you wish to attack");
+                    x = s.nextInt();
+                }
+
+
+            //checks if the position the user guessed is a hit, miss, or already guessed
+            //miss, user doesn't get to guess again
+            if (b1.getIntSquares()[y][x] == 0) {
+                System.out.println("Miss");
+                b1.shoot(y, x);
+                attackAvailable = false;
             }
 
+            //if user already guessed this spot, then they guess again
+            else if (b1.getIntSquares()[y][x] == -1) {
+                System.out.println("You already guessed there silly. Guess again");
+            }
 
+            //user hits a part of the ship and gets to guess again
+            else {
+                System.out.println("JUSTUS HAS BEEN SERVED. (Hit)");
+                b1.shoot(y, x);
 
-
-
-
-
-
-
-        }
-
-        //checks if the position the user guessed is a hit, miss, or already guessed
-        if (b1.getIntSquares()[y][x] == 0) {
-            System.out.println("Miss");
-            b1.shoot(y, x);
-            attackAvailable = false;
-        } else if (b1.getIntSquares()[y][x] == -1) {
-            System.out.println("You already guessed there silly. Guess again");
-            secondTime = true;
-        } else {
-            System.out.println("JUSTUS HAS BEEN SERVED. (Hit)");
-            b1.shoot(y, x);
-
-            //checks if a ship will sink
-            if (b1.checkSunk(y, x, b1)) {
-                System.out.println("SUNK!");
-                otherPlayerSunken++;
+                //checks if a ship will sink
+                if (b1.checkSunk(y, x, b1)) {
+                    System.out.println("SUNK!");
+                    otherPlayerSunken++;
+                }
+//                if (otherPlayerSunken == 5){
+//                    System.out.println("Win");
+//                    attackAvailable = false;
+//                }
+                System.out.println("Guess again");
             }
         }
     }
 }
+
